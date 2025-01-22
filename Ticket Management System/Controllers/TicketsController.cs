b20fb9a306b206b480293dd6cc8a40383ba44a26
SERENDIPITY-19 <pranishaashakya@gmail.com>
2025-01-22 18:36:40 +0545

    Added Create New Tickets and Users button.

---
+++ Ticket Management System/Controllers/TicketsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ticket_Management_System.Controllers
+{
+    public class TicketsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
