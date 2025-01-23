4803a8fd3021ea31b1716ec980f66ba225961010
SERENDIPITY-19 <pranishaashakya@gmail.com>
2025-01-23 19:30:19 +0545

    Created Users page

---
+++ Ticket Management System/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ticket_Management_System.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
