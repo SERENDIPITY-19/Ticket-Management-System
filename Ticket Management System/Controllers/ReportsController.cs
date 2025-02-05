ffb27267321396cda8cba58916aac8b39491f678
SERENDIPITY-19 <pranishaashakya@gmail.com>
2025-02-05 21:23:20 +0545

    Minor fixes from last update.

---
+++ Ticket Management System/Controllers/ReportsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ticket_Management_System.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
